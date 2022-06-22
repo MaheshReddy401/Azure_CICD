f7b47eabf208365bee59528cce809290a82e163f
maheshreddy401 <maheshreddy528@gmail.com>
2022-06-22 19:51:35 +0100

    modified changes 2

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const DEFAULT_EXPENSES = [
   { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
   {
     id: "e3",
-    title: "Car Insurance",
+    title: "Car Insurance modified",
     amount: 294.67,
     date: new Date(2021, 2, 28),
   },
